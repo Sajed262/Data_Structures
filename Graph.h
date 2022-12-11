3534acbd7ba17caecf02a2bb2d35a8d9694f5c02
Sajed <taya.sajed.99@gmail.com>
2022-12-11 15:29:32 +0200

    modified

--- Graph.h
+++ Graph.h
@@ -39,20 +39,20 @@ private:
     // the index of the array represents a vertex and each element is a list of edges, i.e.,
     // the list contains other vertices that form an edge with that vertex. the key will be the other vertices,
     // and the data will be the weight
-    DynamicArray<List<int>> edges;     // if the graph is direct each cell in the array will be vector that contains the IN edges
-    DynamicArray<List<int>> out_edges; // will be used only if the graph is directed.
-    // e.g., if we have an edge 1->2, then in the out_edges at index 1 we will add 2,
-    // and in the edges at index 2 we will add 1.
+    DynamicArray<List<int>> edges;     // if the graph is direct each cell in the array will be vector that contains the out edges
+    DynamicArray<List<int>> in_edges; // will be used only if the graph is directed.
+    // e.g., if we have an edge 1->2, then in the in_edges at index 2 we will add 1,
+    // and in the edges at index 1 we will add 2.
     vector<Edge> weighted_edges;
 
 public:
     // can be used as a none-parameter constructor, but as default the graph will be directed.
     // and can be used a one parameter constructor.
-    explicit Graph(bool directed = true) : is_directed(directed), num_of_vertices(0), edges(), out_edges(), weighted_edges() {}
+    explicit Graph(bool directed = true) : is_directed(directed), num_of_vertices(0), edges(), in_edges(), weighted_edges() {}
 
     // it is your job to fill the vector with edges
     Graph(int vertices, bool is_directed, vector<Edge> ed) : num_of_vertices(vertices), is_directed(is_directed),
-                                                             edges(num_of_vertices), out_edges(num_of_vertices), weighted_edges(ed)
+                                                             edges(num_of_vertices), in_edges(num_of_vertices), weighted_edges(ed)
     {
         for (int i = 0; i < ed.size(); i++)
             addEdge(ed[i].src, ed[i].dest, ed[i].weight);
@@ -70,32 +70,32 @@ public:
         List<int> tmp;
         edges.insert(tmp);
         if (is_directed)
-            out_edges.insert(tmp);
+            in_edges.insert(tmp);
         return num_of_vertices++;
     }
     // there is no remove function for the vertices.
 
     // in case of directed graph, from will be the vertex that the edge comes out of,
     // and to will be the vertex that the edge points to
-    // we dont add the same edge more than once. lets we have 1->2 and 2->1 in the directed graph and we want the undirected graph of it,
+    // we dont add the same edge more than once. lets say we have 1->2 and 2->1 in the directed graph and we want the undirected graph of it,
     // in this case we add 1-2 once in each index. we dont need parallel edges.
     // and in case of 2->2, we add 2-2 once.
     void addEdge(int from, int to, int wei = 1)
     {
         if (from < 0 || to < 0 || from >= num_of_vertices || to >= num_of_vertices)
             return;
-        if (edges[to].getElement(from)) // the case of parallel edges.
+        if (edges[from].getElement(to)) // the case of parallel edges.
             return;
 
-        edges[to].addElement(from, wei);
+        edges[from].addElement(to, wei);
         if (is_directed)
-            out_edges[from].addElement(to, wei);
+            in_edges[to].addElement(from, wei);
 
         else // the graph is undirected
         {
             if (from == to) // the case of 2->2.
                 return;
-            edges[from].addElement(to, wei);
+            edges[to].addElement(from, wei);
         }
 
         Edge tmp(from, to, wei);
@@ -106,11 +106,11 @@ public:
     {
         if (from < 0 || to < 0 || from >= num_of_vertices || to >= num_of_vertices)
             return;
-        edges[to].removeElement(from);
+        edges[from].removeElement(to);
         if (is_directed)
-            out_edges[from].removeElement(to);
+            in_edges[to].removeElement(from);
         else // the graph is undirected
-            edges[from].removeElement(to);
+            edges[to].removeElement(from);
     }
 
     void removeAllEdgesForVertex(int index) // remove all the edges that the vertex number index is at either sides of the edge.
@@ -123,13 +123,13 @@ public:
             {
                 while (edges[index].first)
                     edges[index].removeLast();
-                while (out_edges[index].first)
-                    out_edges[index].removeLast();
+                while (in_edges[index].first)
+                    in_edges[index].removeLast();
                 continue;
             }
             edges[i].removeElement(index);
             if (is_directed)
-                out_edges[i].removeElement(index);
+                in_edges[i].removeElement(index);
         }
     }
 
@@ -138,7 +138,7 @@ public:
         num_of_vertices = 0;
         edges.clear();
         if (is_directed)
-            out_edges.clear();
+            in_edges.clear();
     }
 
     Graph theUndirectedGraph() // if the edges have weights then the weights in the undirected graph wont be correct.
